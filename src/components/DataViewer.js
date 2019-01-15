dd04ec40c43c7e48709fa30494e0f8d281da507f
Connor Tangney <Couch3ater@gmail.com>
2019-01-15 12:10:18 -0500

    update data viewer module

--- src/components/DataViewer.js
+++ src/components/DataViewer.js
@@ -6,6 +6,12 @@ class DataViewer extends Component {
   getData = () => {
     console.log(this.props.couponData);
   }
+  openAdvancedQuery = (event, id) => {
+    for(let i = 0; i < document.getElementsByClassName('advanced-query').length; i++){
+      document.getElementsByClassName('advanced-query')[i].classList.remove('open');
+    }
+    document.getElementById('query'+id).classList.toggle('open');
+  }
   render(){
     return(
       <div>
@@ -14,7 +20,15 @@ class DataViewer extends Component {
             <h1>CoupON Data Viewer</h1>
             <div className='columns'>
               <div className='column col-6 col-mx-auto text-center'>
-                <p>In the Data Viewer module, you can review information like which total number of barcodes issued, total number of barcodes redeemed, information by date, and more. If you need an additional data view, please contact your administrator with your request.</p>
+                <p>In the Data Viewer module, you can review information like the total number of barcodes issued, total number of barcodes redeemed, information by date, and more. If you need an additional data view, please contact your administrator with your request.</p>
+              </div>
+            </div>
+            <div className='columns'>
+              <div className='column col-8 col-mx-auto text-center'>
+                <a className='cumulative' onClick={event => this.openAdvancedQuery(event, 'main')}><span className='coupon-info-label'>Cumulative Data View</span>&nbsp;<span id='icon-cumulative'><i className='icon icon-arrow-down'></i></span></a>
+                <div className='advanced-query' id={'querymain'}>
+                  <p>This feature will allow you to query information stored in all lists.</p>                  
+                </div>
               </div>
             </div>
             {this.props.couponData.map( list =>
@@ -36,8 +50,13 @@ class DataViewer extends Component {
                     </div>
                   </div>
                   <div className='columns'>
-                    <div className='column col-10 col-mx-auto text-center'>
-                      <button className='btn btn-action btn-expand'><i className='icon icon-arrow-down'></i></button>
+                    <div className='column col-8 col-mx-auto text-center'>
+                      <button className='btn btn-action btn-expand' onClick={event => this.openAdvancedQuery(event, list._id)}><i className='icon icon-plus'></i></button>
+                      <div className='advanced-query text-left' id={'query'+list._id}>
+                        <span className='coupon-info-label'>Advanced Data Selector</span>
+                        <p>Use the advanced data selector to view more specific information pertaining to a given list.</p>
+
+                      </div>
                     </div>
                   </div>
                 </div>
